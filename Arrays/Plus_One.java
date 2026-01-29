9ae86af0fea7c5ba444d250799ebf41263e59729
Naveen.Dinesh.Hegde <naveendhegde2006@gmail.com>
2026-01-29 22:19:04 +0530

    Create Plus_One.java

---
+++ Arrays/Plus_One.java
@@ -0,0 +1,22 @@
+class Solution
+ {
+    public int[] plusOne(int[] digits) 
+    {
+        int i;
+        for(i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]==9)
+            {
+                digits[i]=0;
+            }
+            else
+            {
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        int[] ans=new int[digits.length+1];
+        ans[0]=1;
+        return ans;
+    }
+}
