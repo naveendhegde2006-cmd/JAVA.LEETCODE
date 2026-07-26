2af42b5d82eb7e98a7e97fb28e33200d6f4fd810
Naveen.Dinesh.Hegde <naveendhegde2006@gmail.com>
2026-07-26 10:36:21 +0530

    Time: 0 ms (100%), Space: 42.5 MB (56.06%) - LeetHub

---
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestInteger(int n, int s) 
+    {
+        if(s>9*n)
+            return -1;
+        int res=0;
+        for(int i=0;i<n;i++)
+            {
+                int d=Math.min(9,s);
+                res=res*10+d;
+                s-=d;
+            }
+        return res;
+    }
+}
