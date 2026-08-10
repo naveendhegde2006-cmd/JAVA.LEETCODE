eea1ce98bd3aa708e3bea4435b219d3b41c3db94
Naveen.Dinesh.Hegde <naveendhegde2006@gmail.com>
2026-08-10 16:16:59 +0530

    Time: 1 ms (99.95%), Space: 46.9 MB (65.86%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int singleNumber(int[] nums)
+     {
+       int ans=0;
+       for(int i=0;i<nums.length;i++)
+       {
+        ans=ans^nums[i];
+       }
+       return ans;
+    }
+}
