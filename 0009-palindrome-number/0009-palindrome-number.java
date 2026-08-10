4038d449aad0e59466ec56d4773188ae0266b7cc
Naveen.Dinesh.Hegde <naveendhegde2006@gmail.com>
2026-08-10 10:59:36 +0530

    Time: 5 ms (81.19%), Space: 46 MB (53.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x)
+     {
+        int rev=0,d,z=x;
+        while(x!=0)
+        {
+        d=x%10;
+        x=x/10;
+        rev=rev*10+d;
+        }
+        if(z>0 && rev==z || z==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
